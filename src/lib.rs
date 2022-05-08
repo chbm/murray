254c858ddfef9eb04b74f2348c7276c2a738e10e
Carlos Morgado <carlos primatas.org>
2022-05-08 16:07:32 +0100

    better complex type handling

--- src/lib.rs
+++ src/lib.rs
@@ -91,21 +91,6 @@ macro_rules! parse_options {
     }
 }
 // Group { delimiter: Brace, stream: TokenStream [Ident { ident: "sup", span: #0 bytes(697..700) }, Punct { ch: ':', spacing: Alone, span: #0 bytes(700..701) }, Ident { ident: "None", span: #0 bytes(702..706) }, Punct { ch: ',', spacing: Alone, span: #0 bytes(706..707) }, Ident { ident: "id", span: #0 bytes(716..718) }, Punct { ch: ':', spacing: Alone, span: #0 bytes(718..719) }, Ident { ident: "u32", span: #0 bytes(720..723) }, Punct { ch: ',', spacing: Alone, span: #0 bytes(723..724) }], span: #0 bytes(687..730) }
-
-
-
-macro_rules! parse_block {
-    ($i:expr) => {
-        {
-            consume_punct!($i);
-            let r = match $i.next() {
-                Some(TokenTree::Group(block)) => block.stream(),
-                _ => fail!("expected block")
-            };
-            r.into()
-        }
-    }
-}
 //Some(Group { delimiter: Brace, stream: TokenStream [Ident { ident: "A", span: #0 bytes(756..757) }, Punct { ch: ',', spacing: Alone, span: #0 bytes(757..758) }, Ident { ident: "B", span: #0 bytes(76 7..768) }, Group { delimiter: Brace, stream: TokenStream [Ident { ident: "x", span: #0 bytes(783..784) }, Punct { ch: ':', spacing: Alone, span: #0 bytes(784..785) }, Punct { ch: '&', spacing: Alone , span: #0 bytes(786..787) }, Ident { ident: "str", span: #0 bytes(787..790) }, Punct { ch: ',', spacing: Alone, span: #0 bytes(790..791) }], span: #0 bytes(769..801) }, Punct { ch: ',', spacing: Al one, span: #0 bytes(801..802) }], span: #0 bytes(746..808) }) unexpected Punct { ch: ',', spacing: Alone, span: #0 bytes(808..809) }
 
 macro_rules! get_block {
@@ -113,9 +98,44 @@ macro_rules! get_block {
         {
             consume_punct!($i);
             match $i.next() {
-                Some(TokenTree::Group(block)) => block.stream().into_iter(),
+                Some(TokenTree::Group(block)) => {
+                    block.stream().into_iter()
+                },
                 _ => fail!("expected block")
             }
+        }
+    }
+}
+
+macro_rules! parse_type {
+    ($i:expr) => {
+        {
+            let mut tokens : Vec<TokenTree> = Vec::new();
+            let mut depth = 0;
+            while let Some(e) = $i.next() {
+                match e {
+                    TokenTree::Ident(x) => {
+                        tokens.push(TokenTree::Ident(x))
+                    },
+                    TokenTree::Punct(x) => {
+                        match x.as_char() {
+                            ',' => if depth == 0 {
+                                break; 
+                            },
+                            '<' | '(' | '[' | '{' => { depth = depth +1 },
+                            '>' | ')' | ']' | '}' => { depth = depth -1 }, 
+                            _ => {} // neutral punct : 
+                        } 
+                        tokens.push(TokenTree::Punct(x))
+                    },
+                    TokenTree::Group(x) => tokens.push(TokenTree::Group(x)),
+                    _ => {
+                        eprintln!("your error is near {:?}", e);
+                        fail!("unexpected token in actor state member type");
+                    }
+                }
+            }
+            TokenStream::from_iter(tokens.into_iter())
         }
     }
 }
@@ -128,12 +148,7 @@ macro_rules! parse_state {
                 match e {
                     TokenTree::Ident(name) => {
                         consume_punct!(ii); // :
-                        match ii.next() {
-                            Some(TokenTree::Ident(val)) => {
-                                $v.push((name.to_string(), val.to_string()));
-                            },
-                            _ => fail!("bad type in actor state definition")
-                        }
+                        $v.push((name.to_string(), parse_type!(ii)));
                     },
                     TokenTree::Punct(_) => {}, // ignore , 
                     _ => fail!("unexpected token in actor state definition")
@@ -199,7 +214,7 @@ pub fn actor(tokens: TokenStream) -> TokenStream {
   
     let mut messages_structs = quote!{};
     let mut messages_block = quote!{};
-    let mut extra_state : Vec<(String,String)> = Vec::new();
+    let mut extra_state : Vec<(String,TokenStream)> = Vec::new();
     let mut messages : Vec<(String, Option<TokenStream>)> = Vec::new();
     let mut messages_match_block = quote!{};
 
@@ -243,8 +258,9 @@ pub fn actor(tokens: TokenStream) -> TokenStream {
     for (name, group) in messages {
         let n = format_ident!("{}", name);
         let sn = format_ident!("{}{}", messages_ident, name);
-        let hn = format_ident!("handle_{}", name);
+        let hn = format_ident!("handle_{}", name.to_lowercase());
         if let Some(g) = group {
+        #[allow(unused_assignments)]
             let mut x = quote!{};
             x = g.into(); // coerse into token_macro2
             messages_structs = quote! { #messages_structs 
@@ -266,7 +282,9 @@ pub fn actor(tokens: TokenStream) -> TokenStream {
 
     for (name, typ) in extra_state {
         let n = format_ident!("{}", name);
-        let t = format_ident!("{}", typ);
+        #[allow(unused_assignments)]
+        let mut t = quote! {};
+        t = typ.into();
         state_opts_block = quote! { #state_opts_block #n : Option<#t> , };
         state_init_opts_block = quote! { #state_init_opts_block #n : None , };
     }
